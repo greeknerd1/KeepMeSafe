be97eb3662d26566d352b44644235ac39973d13f
Gabriel Khouri-Haddad <gabykhh@gmail.com>
2018-07-28 11:13:40 -0700

    Added a tableview to show contacts on the first page

--- KeepMeSafe/Controllers/ConfigureViewController.swift
+++ KeepMeSafe/Controllers/ConfigureViewController.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2018 Gabriel Khouri-Haddad. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ConfigureViewController: UIViewController {
+class ConfigureViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var contacts = [Contact]()
+        
+    @IBOutlet weak var scrollableTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//         Do any additional setup after loading the view, typically from a nib.
+        scrollableTableView.delegate = self
+        scrollableTableView.dataSource = self
+        ContactService.contacts(for: User.current) { (contacts) in
+            self.contacts = contacts
+            self.scrollableTableView.reloadData()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        ContactService.contacts(for: User.current) { (contacts) in
+            self.contacts = contacts
+            self.scrollableTableView.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,20 @@ class ConfigureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ScrollableContactCell", for: indexPath) as! ScrollableContactCell
+
+        let contact = contacts[indexPath.row]
+        let name = contact.name
+
+        cell.nameLabel.text = name
+
+        return cell
+    }
     
 }
 
---
+++ KeepMeSafe/Views/ScrollableContactCell.swift
@@ -0,0 +1,19 @@
+//
+//  ScrollableContactCell.swift
+//  KeepMeSafe
+//
+//  Created by Gabriel Khouri-Haddad on 7/27/18.
+//  Copyright © 2018 Gabriel Khouri-Haddad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScrollableContactCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
