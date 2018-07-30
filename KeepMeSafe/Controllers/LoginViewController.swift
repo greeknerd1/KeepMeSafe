89c594ea0aedfab051b33ac6307778c38f5aa4fc
Gabriel Khouri-Haddad <gabykhh@gmail.com>
2018-07-30 11:44:30 -0700

    Fixed bug that crashes the sign-in

--- KeepMeSafe/Controllers/LoginViewController.swift
+++ KeepMeSafe/Controllers/LoginViewController.swift
@@ -42,7 +42,7 @@ extension LoginViewController: FUIAuthDelegate {
     func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
         
         if let error = error {
-            assertionFailure("Error signing in: \(error.localizedDescription)")
+            //assertionFailure("Error signing in: \(error.localizedDescription)")
             return
         }
         
