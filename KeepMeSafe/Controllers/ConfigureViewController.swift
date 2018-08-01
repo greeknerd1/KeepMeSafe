b0ca872e8dc8b07d556c9b7ead901c73ee7f3085
Gabriel Khouri-Haddad <gabykhh@gmail.com>
2018-07-31 17:28:48 -0700

    Added location being incorporated into the message

--- KeepMeSafe/Controllers/ConfigureViewController.swift
+++ KeepMeSafe/Controllers/ConfigureViewController.swift
@@ -8,10 +8,15 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class ConfigureViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ConfigureViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
     
     var contacts = [Contact]()
+    let manager = CLLocationManager()
+    
+    var mainStringToDisplay = "USER is deemed unsafe and may be in danger. You are one of their emergency contacts. Their current location is: "
+    var locationStringToDisplay = "LOCATION"
         
     @IBOutlet weak var scrollableTableView: UITableView!
     @IBOutlet weak var messageTextLabel: UILabel!
@@ -25,7 +30,51 @@ class ConfigureViewController: UIViewController, UITableViewDataSource, UITableV
             self.contacts = contacts
             self.scrollableTableView.reloadData()
         }
-        messageTextLabel.text = "\(User.current.username) is deemed unsafe and may be in danger. You are one of their emergency contacts. Their current location is: LOCATION"
+        
+        //Location code
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        mainStringToDisplay = "\(User.current.username) is deemed unsafe and may be in danger. You are one of their emergency contacts. Their current location is: "
+        messageTextLabel.text = "\(mainStringToDisplay) \(locationStringToDisplay)"
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemark, error) in
+            if error != nil {
+                print("There was an error with the Geocoder().reverse")
+            }
+            else {
+                if let place = placemark?[0] {
+                    //place.postalAddress! <-- this contains all the relevant location information
+                
+                    if let name = place.name {
+                       self.locationStringToDisplay = name
+                    }
+                    if let subLocality = place.subLocality {
+                        self.locationStringToDisplay.append(", \(subLocality)")
+                    }
+                    if let locality = place.locality {
+                        self.locationStringToDisplay.append(", \(locality)")
+                    }
+                    if let adminArea = place.administrativeArea {
+                        self.locationStringToDisplay.append(", \(adminArea)")
+                    }
+                    if let postalCode = place.postalCode {
+                        self.locationStringToDisplay.append(", \(postalCode)")
+                    }
+                    if let countryCode = place.isoCountryCode {
+                        self.locationStringToDisplay.append(", \(countryCode)")
+                    }
+                    print("location updated")
+                    self.messageTextLabel.text = "\(self.mainStringToDisplay) \(self.locationStringToDisplay)"
+                }
+            }
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -38,12 +87,13 @@ class ConfigureViewController: UIViewController, UITableViewDataSource, UITableV
     @IBAction func editMessageButtonPressed(_ sender: UIButton) {
         let addAlert = UIAlertController(title: "Emergency Message", message: "Edit your message", preferredStyle: .alert)
         addAlert.addTextField { (textField: UITextField) in
-            textField.placeholder  = "\(User.current.username) is deemed unsafe and may be in danger. You are one of their emergency contacts. Their current location is: LOCATION" //change to get their location
+            textField.placeholder  = "\(self.mainStringToDisplay) \(self.locationStringToDisplay)"
         }
         addAlert.addAction(UIAlertAction(title: "Save", style: .default, handler: { (action:UIAlertAction) in
             guard let message = addAlert.textFields?.first?.text else { return }
             if message != "" {
-                self.messageTextLabel.text = message
+                self.mainStringToDisplay = message
+                self.messageTextLabel.text = "\(self.mainStringToDisplay) \(self.locationStringToDisplay)"
             }
         }))
         addAlert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
